5774b60ec15cb79e3bd51d4a2956d71d1c835400
danil <kotikov.lil@bkl.ru>
2024-08-06 11:52:44 +0300

    Исправил все ошибки

--- src/app.py
+++ src/app.py
@@ -19,10 +19,5 @@ async def startup_event():
     await model_manager.create_test_user()
 
 
-@app.get("/", response_class=HTMLResponse)
-async def get_root(request: Request) -> HTMLResponse:
-    return HTMLResponse("API is Working")
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
